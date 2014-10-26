f3aa9e24baaa5de305cc03037d32b514d64fb4df
Jordan Stone <jmstone617@gmail.com>
2014-10-26 12:36:04 -0600

    Initial commit

---
+++ tempoiq_export.rb
@@ -0,0 +1,40 @@
+#
+# Example:
+#
+# TIQ_EXPORT_DIR='export' TIQ_START='2013-01-01' TIQ_END='2014-12-31' TIQ_HOST='...' TIQ_API_KEY='...' TIQ_API_SECRET='...' ruby export.rb
+#
+#
+
+require 'rubygems'
+require 'tempoiq'
+require 'json'
+require 'csv'
+require 'fileutils'
+require 'time'
+
+export_dir = ENV['TIQ_EXPORT_DIR']
+start_on = Time.parse(ENV['TIQ_START'])
+end_on = Time.parse(ENV['TIQ_END'])
+
+tiq = TempoIQ::Client.new(ENV['TIQ_API_KEY'], ENV['TIQ_API_SECRET'], ENV['TIQ_HOST'])
+
+tiq.list_devices.each do |device|
+	puts "Exporting [#{device.id}]: #{device.key}"
+
+	meta_file = File.join(export_dir, device.key, 'meta.json')
+	data_file = File.join(export_dir, device.key, 'data.csv')
+
+	FileUtils.mkdir_p(File.dirname(meta_file))
+  File.open(meta_file, 'w') do |f|
+    f.puts device.to_json
+  end
+
+  CSV.open(data_file, 'w') do |csv|
+    csv << %w[timestamp value]
+    cursor = tiq.read({devices: {key: device.key}, sensors: { key: 'accel_x' }}, start_on, end_on)
+    puts "Found #{cursor.count} data points"
+    cursor.each do |dp|
+      csv << [dp.ts, dp.value]
+    end
+  end
+end
