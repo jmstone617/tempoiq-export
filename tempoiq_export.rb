fd953a9be00d191dade2c3b813e2b0002ae2cbe3
Jordan Stone <jmstone617@gmail.com>
2015-06-16 22:38:58 -0600

    Adds ability to query for a specific device key. Also raises errors if API keys or host are missing

--- tempoiq_export.rb
+++ tempoiq_export.rb
@@ -16,35 +16,68 @@ export_dir = ENV['TIQ_EXPORT_DIR']
 start_on = Time.parse(ENV['TIQ_START'])
 end_on = Time.parse(ENV['TIQ_END'])
 user_id = ENV['USER_ID']
+device_key = ENV['DEVICE_KEY']
+
+raise ArgumentError.new('TempoIQ API key is missing') unless ENV['TIQ_API_KEY']
+raise ArgumentError.new('TempoIQ API secret is missing') unless ENV['TIQ_API_SECRET']
+raise ArgumentError.new('TempoIQ host is missing') unless ENV['TIQ_HOST']
 
 tiq = TempoIQ::Client.new(ENV['TIQ_API_KEY'], ENV['TIQ_API_SECRET'], ENV['TIQ_HOST'])
 
-# devices: {:attributes => {'user_id' => '13'}}
-devices = nil
-if user_id
-  devices = {:attributes => {'user_id' => user_id}}
+if device_key
+  device = tiq.get_device(device_key)
+  if device
+    puts "Exporting [#{device.name}]: #{device.key}"
+
+    meta_file = File.join(export_dir, device.key, 'meta.json')
+    data_file = File.join(export_dir, device.key, 'data.csv')
+
+    FileUtils.mkdir_p(File.dirname(meta_file))
+    File.open(meta_file, 'w') do |f|
+      f.puts device.to_json
+    end
+
+    CSV.open(data_file, 'w') do |csv|
+      csv << %w[timestamp value key]
+      cursor = tiq.read({devices: {key: device.key}, sensors: 'all'}, start_on, end_on)
+      cursor.each do |row|
+        values = row.values[device.key]
+        values.keys.each do |key|
+          csv << [row.ts, row.value(device.key, key), key]
+        end
+      end
+    end
+  else
+    puts "Unable to find device with key #{device_key}"
+  end
 else
-  devices = 'all'
-end
-
-tiq.list_devices(devices: devices).each do |device|
-  puts "Exporting [#{device.name}]: #{device.key}"
-
-  meta_file = File.join(export_dir, device.key, 'meta.json')
-  data_file = File.join(export_dir, device.key, 'data.csv')
-
-  FileUtils.mkdir_p(File.dirname(meta_file))
-  File.open(meta_file, 'w') do |f|
-    f.puts device.to_json
+  # devices: {:attributes => {'user_id' => '13'}}
+  devices = nil
+  if user_id
+    devices = {:attributes => {'user_id' => user_id}}
+  else
+    devices = 'all'
   end
 
-  CSV.open(data_file, 'w') do |csv|
-    csv << %w[timestamp value key]
-    cursor = tiq.read({devices: {key: device.key}, sensors: 'all'}, start_on, end_on)
-    cursor.each do |row|
-      values = row.values[device.key]
-      values.keys.each do |key|
-        csv << [row.ts, row.value(device.key, key), key]
+  tiq.list_devices(devices: devices).each do |device|
+    puts "Exporting [#{device.name}]: #{device.key}"
+
+    meta_file = File.join(export_dir, device.key, 'meta.json')
+    data_file = File.join(export_dir, device.key, 'data.csv')
+
+    FileUtils.mkdir_p(File.dirname(meta_file))
+    File.open(meta_file, 'w') do |f|
+      f.puts device.to_json
+    end
+
+    CSV.open(data_file, 'w') do |csv|
+      csv << %w[timestamp value key]
+      cursor = tiq.read({devices: {key: device.key}, sensors: 'all'}, start_on, end_on)
+      cursor.each do |row|
+        values = row.values[device.key]
+        values.keys.each do |key|
+          csv << [row.ts, row.value(device.key, key), key]
+        end
       end
     end
   end
